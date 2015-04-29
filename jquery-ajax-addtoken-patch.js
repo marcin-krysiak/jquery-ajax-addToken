05ada95baaf0c453750d33645c2b472c03239c80
Marcin Krysiak <marcin.krysiak@gmail.com>
2015-04-29 15:05:31 +0100

    Update jquery-ajax-addtoken-patch.js

--- jquery-ajax-addtoken-patch.js
+++ jquery-ajax-addtoken-patch.js
@@ -7,7 +7,8 @@
 jQuery.extend({
 
   originalAjax: originalAjax,
-  tokenUrl: 'getToken',
+  tokenUrl: 'getToken', //in case you want to request token from server and add it to the header prior to each request
+  fixedToken: '', //in case if you have fixed token and do not need to request from server every time. Leave empty string, null or false to deactivate or put some string value to make it active and ignore server requesting for token
   requestsWithToken: ['GET', 'PUT', 'POST', 'DELETE'],
   tokenDataField: 'ownToken',
   tokenHeader: 'Own-Token',
@@ -27,24 +28,30 @@ jQuery.extend({
         options = options || {};
     /***********************************************/
   
-    if ( options.url === this.tokenUrl ) && requestsWithToken.indexOf( options.type.toUpperCase() ) !== -1 ) {
-  
+    if ( requestsWithToken.indexOf( options.type.toUpperCase() ) !== -1 && !this.fixedToken ) {
+     
       this.originalAjax({
         type: "GET",
-        url: 'token'
+        url: tokenUrl
       }).done( function(data) {
         options.headers = options.headers || {};
         options.headers[this.tokenHeader] = data[tokenDataField];
   
         self.originalAjax(url, options)
-          .done( function(data) {deferObj.resolve(data)})
-          .fail( function(data) {deferObj.reject(data)});
+         .done( function(data) {deferObj.resolve(data)})
+         .fail( function(data) {deferObj.reject(data)});
       });
 
     } else {
-      self.originalAjax(url, options)
-        .done( function(data) {deferObj.resolve(data)})
-        .fail( function(data) {deferObj.reject(data)});
+     
+     if ( fixedToken ) {
+      options.headers = options.headers || {};
+      options.headers[this.tokenHeader] = fixedToken;
+     }
+     
+     self.originalAjax(url, options)
+      .done( function(data) {deferObj.resolve(data)})
+      .fail( function(data) {deferObj.reject(data)});
     }
 
     return deferObj.promise();
